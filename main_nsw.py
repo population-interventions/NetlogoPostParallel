034906ced49033e9192dd2b35b8bc2d20910bb60
Tim Wilson <timedwilson@gmail.com>
2021-09-11 15:39:01 +1000

    Add index for NSW.

---
+++ main_nsw.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import sys
+
+from processNetlogoOutput import DoAbmProcessing
+
+measureCols_raw = [
+	'cont_stage',
+	'vac_lockdown_ease',
+]
+measureCols = [
+	'Stage',
+	'VacEase',
+]
+
+def indexRenameFunc(chunk):
+	index = chunk.index.to_frame()
+	#index['R0'] = index['global_transmissibility_out'].apply(lambda x: 3.75 if x < 0.61333 else (4.17 if x < 0.681666 else 4.58))
+
+	renameCols = {measureCols_raw[i] : measureCols[i] for i in range(len(measureCols))}
+	index = index.rename(columns=renameCols)
+	
+	chunk.index = pd.MultiIndex.from_frame(index)
+	return chunk
+
+
+inputDir = 'outputs_snowy/'
+outputDir = 'outputs_post/'
+day_override = False
+
+
+def main(arrayIndex):
+	DoAbmProcessing(inputDir, outputDir, arrayIndex, indexRenameFunc, measureCols, measureCols_raw, day_override=day_override)
+
+
+if __name__== "__main__":
+	# length check for testing with no arguments.
+	main(sys.argv[1] if len(sys.argv) > 1 else 1)
